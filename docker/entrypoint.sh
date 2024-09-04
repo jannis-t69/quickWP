dc453f1f6b63243b75ea24fc8a43b2000e3dd952
jannis-t69 <jannis.t69@@gmail.com>
2024-09-04 19:40:52 +0300

    Commit

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -36,8 +36,15 @@ if [ "$QUICKWP_REINSTALL" = 1 ]; then
     /usr/local/bin/wp core download --allow-root --path=/var/www/html --force --locale=$QUICKWP_LOCALE
 
     /usr/local/bin/wp config create --allow-root --dbname=wordpress --dbuser=quickwp --dbpass=quickwp --extra-php <<PHP
+// Enable WP_DEBUG mode
 define( 'WP_DEBUG', true );
+// Enable Debug logging to the /wp-content/debug.log file
 define( 'WP_DEBUG_LOG', true );
+// Disable display of errors and warnings
+define( 'WP_DEBUG_DISPLAY', false );
+@ini_set( 'display_errors', 0 );
+// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
+define( 'SCRIPT_DEBUG', false );
 define( 'WP_HOME', "http://$QUICKWP_URL:$QUICKWP_PORT" );
 define( 'WP_SITEURL', "http://$QUICKWP_URL:$QUICKWP_PORT" );
 PHP
